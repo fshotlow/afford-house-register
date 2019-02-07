0cde4904d48eac57472b5af0f4132c67482ab7a6
Frederick Shotlow <frederick.shotlow@austintexas.gov>
2019-02-07 09:42:38 -0600

    add static client build path

--- app.js
+++ app.js
@@ -11,10 +11,11 @@ var usersRouter = require('./routes/Users')
 
 var app = express();
 
-
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
+
+app.use(express.static(path.join(__dirname, “client/build”)))
 
 app.use(logger('dev'));
 // app.use(express.json());
